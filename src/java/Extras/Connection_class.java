7717017baeafadb2c99ef66038c270e76e23a421
Pablo Patarca <pablopatarca@gmail.com>
2013-10-02 15:39:58 -0300

    Modificacin de string connection

--- src/java/Extras/Connection_class.java
+++ src/java/Extras/Connection_class.java
@@ -6,11 +6,13 @@ public class Connection_class{
   public static Connection mysql_connect() {
       Connection conn = null;
       String url = "jdbc:mysql://localhost:3306/";
-      //String url = "jdbc:mysql://https://mysql-proyectojavautn.jelastic.servint.net";
+      //String url = "jdbc:mysql://mysql-proyecto-java.jelastic.servint.net/";
       String dbName = "jdelivery_db";
       String driver = "com.mysql.jdbc.Driver";
       String userName = "jdelivery"; 
       String password = "2013";
+      //String userName = "root"; 
+      //String password = "hv5XS8my4f";
       try {
           Class.forName(driver).newInstance();
           conn = DriverManager.getConnection(url+dbName,userName,password);
